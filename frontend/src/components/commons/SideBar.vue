e9d42b9e82faeaa8460b4e8aa3b6587039927ff2
Ahmet Polat <polat2000@gmail.com>
2021-11-26 13:50:13 +0100

    init SideBar

--- frontend/src/components/commons/SideBar.vue
+++ frontend/src/components/commons/SideBar.vue
@@ -1,42 +1,68 @@
 <template>
-    <v-navigation-drawer permanent expand-on-hover>
-        <v-list>
-            <v-list-item class="px-2">
-                <v-list-item-avatar>
-                    <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
-                </v-list-item-avatar>
-            </v-list-item>
+    <v-navigation-drawer permanent expand-on-hover class="primary-dark" width="17rem">
+        <!-- expand-on-hover -->
+        <div class="side-bar-inner-wrapper">
+            <div>
+                <v-list>
+                    <v-list-item class="px-2">
+                        <v-list-item-avatar>
+                            <v-img src="../../static/aktionSodisSmall.png"></v-img>
+                        </v-list-item-avatar>
+                    </v-list-item>
+                </v-list>
 
-            <v-list-item link>
-                <v-list-item-content>
-                    <v-list-item-title class="text-h6">Lorem ipsum</v-list-item-title>
-                    <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list>
+                <v-list nav dense class="mt-12">
+                    <v-list-item-group v-model="model" active-class="primary darken-3">
+                        <v-list-item :to="{ name: 'Home' }">
+                            <v-list-item-icon>
+                                <v-icon color="white">mdi-home-outline</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title class="white--text text-body-1">Home</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item link>
+                            <v-list-item-icon>
+                                <v-icon color="white">mdi-domain</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title class="white--text text-body-1">Base data</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item link>
+                            <v-list-item-icon>
+                                <v-icon color="white">mdi-clipboard-text-outline</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title
+                                class="white--text text-body-1"
+                            >Organization structure</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item link>
+                            <v-list-item-icon>
+                                <v-icon color="white">mdi-wrench-outline</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title class="white--text text-body-1">Technologies</v-list-item-title>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </div>
 
-        <v-divider></v-divider>
-
-        <v-list nav dense>
-            <v-list-item link>
-                <v-list-item-icon>
-                    <v-icon>mdi-folder</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>My Files</v-list-item-title>
-            </v-list-item>
-            <v-list-item link>
-                <v-list-item-icon>
-                    <v-icon>mdi-account-multiple</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Shared with me</v-list-item-title>
-            </v-list-item>
-            <v-list-item link>
-                <v-list-item-icon>
-                    <v-icon>mdi-star</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Starred</v-list-item-title>
-            </v-list-item>
-        </v-list>
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-icon>
+                        <v-icon color="white">mdi-cog-outline</v-icon>
+                    </v-list-item-icon>
+                </v-list-item>
+                <v-list-item class="px-2">
+                    <v-list-item-avatar>
+                        <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
+                    </v-list-item-avatar>
+                    <div class="next-to-avatar">
+                        <v-list-item-content>
+                            <v-list-item-title class="text-h6 white--text">Lorem ipsum</v-list-item-title>
+                            <v-list-item-subtitle class="white--text">sandra_a88@gmail.com</v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-icon color="white">mdi-exit-to-app</v-icon>
+                    </div>
+                </v-list-item>
+            </v-list>
+        </div>
     </v-navigation-drawer>
 </template>
 
@@ -53,3 +79,19 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+.side-bar-inner-wrapper {
+    height: 100vh;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+}
+
+.next-to-avatar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+}
+</style>
