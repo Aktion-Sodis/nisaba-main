da2ce2f842dc1d4154ef67ec26ef2193fdcf1ffc
Ahmet Polat <polat2000@gmail.com>
2021-11-26 18:47:49 +0100

    center the nav icons

--- admin-app/frontend/src/components/commons/SideBar.vue
+++ admin-app/frontend/src/components/commons/SideBar.vue
@@ -2,55 +2,53 @@
     <v-navigation-drawer permanent expand-on-hover class="primary-dark" width="17rem">
         <!-- expand-on-hover -->
         <div class="side-bar-inner-wrapper">
-            <div>
-                <v-list>
-                    <v-list-item class="px-2">
-                        <v-list-item-avatar>
-                            <v-img src="../../static/aktionSodisSmall.png"></v-img>
-                        </v-list-item-avatar>
-                    </v-list-item>
-                </v-list>
+            <v-list>
+                <v-list-item class="px-2">
+                    <v-list-item-avatar>
+                        <v-img src="../../static/aktionSodisSmall.png"></v-img>
+                    </v-list-item-avatar>
+                </v-list-item>
+            </v-list>
 
-                <v-list nav dense class="mt-12">
-                    <v-list-item
-                        :to="{ name: 'Home' }"
-                        exact
-                        :class="currentRouteName === 'Home' ? 'primary darken-4' : ''"
-                    >
-                        <v-list-item-icon>
-                            <v-icon color="white">mdi-home-outline</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title class="white--text text-body-1">Home</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item
-                        :to="{ name: 'BaseData' }"
-                        :class="currentRouteName === 'BaseData' ? 'primary darken-4' : ''"
-                    >
-                        <v-list-item-icon>
-                            <v-icon color="white">mdi-domain</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title class="white--text text-body-1">Base data</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item
-                        :to="{ name: 'OrganizationStructure' }"
-                        :class="currentRouteName === 'OrganizationStructure' ? 'primary darken-4' : ''"
-                    >
-                        <v-list-item-icon>
-                            <v-icon color="white">mdi-clipboard-text-outline</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title class="white--text text-body-1">Organization structure</v-list-item-title>
-                    </v-list-item>
-                    <v-list-item
-                        :to="{ name: 'Technologies' }"
-                        :class="currentRouteName === 'Technologies' ? 'primary darken-4' : ''"
-                    >
-                        <v-list-item-icon>
-                            <v-icon color="white">mdi-wrench-outline</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title class="white--text text-body-1">Technologies</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </div>
+            <v-list nav dense class="mt-12">
+                <v-list-item
+                    :to="{ name: 'Home' }"
+                    exact
+                    :class="currentRouteName === 'Home' ? 'primary darken-4' : ''"
+                >
+                    <v-list-item-icon>
+                        <v-icon color="white">mdi-home-outline</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title class="white--text text-body-1">Home</v-list-item-title>
+                </v-list-item>
+                <v-list-item
+                    :to="{ name: 'BaseData' }"
+                    :class="currentRouteName === 'BaseData' ? 'primary darken-4' : ''"
+                >
+                    <v-list-item-icon>
+                        <v-icon color="white">mdi-domain</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title class="white--text text-body-1">Base data</v-list-item-title>
+                </v-list-item>
+                <v-list-item
+                    :to="{ name: 'OrganizationStructure' }"
+                    :class="currentRouteName === 'OrganizationStructure' ? 'primary darken-4' : ''"
+                >
+                    <v-list-item-icon>
+                        <v-icon color="white">mdi-clipboard-text-outline</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title class="white--text text-body-1">Organization structure</v-list-item-title>
+                </v-list-item>
+                <v-list-item
+                    :to="{ name: 'Technologies' }"
+                    :class="currentRouteName === 'Technologies' ? 'primary darken-4' : ''"
+                >
+                    <v-list-item-icon>
+                        <v-icon color="white">mdi-wrench-outline</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title class="white--text text-body-1">Technologies</v-list-item-title>
+                </v-list-item>
+            </v-list>
 
             <v-list dense>
                 <v-list-item link>
