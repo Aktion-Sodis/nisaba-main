a9469f2f60ec2cf26655869639d85b4b1a97bcdb
Ahmet Polat <polat2000@gmail.com>
2021-11-28 16:28:50 +0100

    add search box & lang selector

--- admin-app/frontend/src/App.vue
+++ admin-app/frontend/src/App.vue
@@ -2,7 +2,30 @@
   <v-app>
     <SideBar v-if="isAuthenticated" />
 
-    <v-main>
+    <div class="search-bar-wrapper">
+      <!-- Language switch is planned only for development -->
+      <v-select
+        v-model="$root.$i18n.locale"
+        :items="langs"
+        item-text="name"
+        item-value="abbr"
+        label="Change lang"
+        outlined
+        dense
+        class="lang-select"
+        style="margin-right: 1rem;"
+      ></v-select>
+      <v-text-field
+        :label="$t('general.search-box')"
+        prepend-inner-icon="mdi-magnify"
+        outlined
+        dense
+        background-color="grey"
+        dark
+      ></v-text-field>
+    </div>
+
+    <v-main class="ml-16">
       <router-view />
     </v-main>
   </v-app>
@@ -15,7 +38,9 @@ import SideBar from './components/commons/SideBar.vue'
 export default {
   name: "App",
   components: { SideBar },
-  data: () => ({}),
+  data: () => ({
+    langs: [{ name: 'English US', abbr: 'en-US' }, { name: 'Spanish Spain', abbr: 'es-ES' }]
+  }),
   computed: {
     ...mapGetters({
       isAuthenticated: "auth/getIsAuthenticated",
@@ -23,3 +48,18 @@ export default {
   },
 };
 </script>
+
+
+<style scoped>
+.search-bar-wrapper {
+  position: absolute;
+  top: 32px;
+  right: 32px;
+  z-index: 1;
+  display: flex;
+}
+
+.lang-select {
+  max-width: 11rem;
+}
+</style>
