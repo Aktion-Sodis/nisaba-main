2ae5da8606f524485f78c1ef69a6b596394f09bb
Ahmet Polat <polat2000@gmail.com>
2021-12-29 22:59:02 +0100

    init vuex module for interventions view gui interactions

---
+++ admin-app/frontend/src/store/interventionsGUIModule/index.js
@@ -0,0 +1,71 @@
+const interventionsModule = {
+  namespaced: true,
+  state: () => ({
+    interventionIdCurrentlyBeingEdited: null,
+    interventionModalIsDisplayed: false,
+  }),
+  getters: {
+    getInterventionModalIsEdit: (state) =>
+      state.InterventionIdCurrentlyBeingEdited !== null,
+    getInterventionCurrentlyBeingEdited: (
+      state,
+      getters,
+      rootState,
+      rootGetters
+    ) =>
+      rootGetters["iv/getInterventionById"](
+        state.interventionIdCurrentlyBeingEdited
+      ) || null,
+    getInterventionModalIsDisplayed: (state) =>
+      state.interventionModalIsDisplayed,
+  },
+  mutations: {
+    setInterventionIdCurrentlyBeingEdited: (state, interventionId) => {
+      state.interventionIdCurrentlyBeingEdited = interventionId;
+    },
+    setInterventionModalIsDisplayed: (state, payload) => {
+      state.interventionModalIsDisplayed = payload;
+    },
+  },
+  actions: {
+    resetAll: ({ commit }) => {
+      commit("setInterventionIdCurrentlyBeingEdited", null);
+      commit("setInterventionModalIsDisplayed", false);
+    },
+    clickOnEditIntervention: ({ commit, dispatch }, payload) => {
+      dispatch("resetAll");
+      commit("setInterventionIdCurrentlyBeingEdited", payload);
+      dispatch("showInterventionModal");
+    },
+    clickOnAddNewIntervention: ({ commit, dispatch }) => {
+      dispatch("resetAll");
+      dispatch("showInterventionModal");
+    },
+    saveIntervention: ({ commit }, { interventionId, name, description }) => {
+      if (interventionId === null)
+        commit(
+          "iv/addIntervention",
+          {
+            name,
+            description,
+          },
+          { root: true }
+        );
+      else
+        commit(
+          "iv/replaceIntervention",
+          {
+            interventionId,
+            name,
+            description,
+          },
+          { root: true }
+        );
+    },
+    showInterventionModal: ({ commit }) => {
+      commit("setInterventionModalIsDisplayed", true);
+    },
+  },
+};
+
+export default interventionsModule;
