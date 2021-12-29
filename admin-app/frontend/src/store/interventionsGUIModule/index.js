5f36a225e03662e939d01f0910a7974150aeaea4
Ahmet Polat <polat2000@gmail.com>
2021-12-29 23:02:50 +0100

    remove redundant commit param

--- admin-app/frontend/src/store/interventionsGUIModule/index.js
+++ admin-app/frontend/src/store/interventionsGUIModule/index.js
@@ -37,7 +37,7 @@ const interventionsModule = {
       commit("setInterventionIdCurrentlyBeingEdited", payload);
       dispatch("showInterventionModal");
     },
-    clickOnAddNewIntervention: ({ commit, dispatch }) => {
+    clickOnAddNewIntervention: ({ dispatch }) => {
       dispatch("resetAll");
       dispatch("showInterventionModal");
     },
