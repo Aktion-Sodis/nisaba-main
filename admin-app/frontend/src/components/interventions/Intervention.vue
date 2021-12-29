bb4435c5002d2fda5812f7f825d4aa926debdc90
Ahmet Polat <polat2000@gmail.com>
2021-12-29 15:14:44 +0100

    init component: Intervention

---
+++ admin-app/frontend/src/components/interventions/Intervention.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card style="height: 100%" class="pa-2" outlined tile>
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+    <v-card-title>
+      {{ interventionName }}
+    </v-card-title>
+    <v-card-subtitle>
+      {{ interventionDescription }}
+    </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+import { validate as uuidValidate } from "uuid";
+
+export default {
+  name: "Intervention",
+  props: {
+    interventionId: {
+      required: true,
+      validator: (e) => uuidValidate(e) || e === null,
+    },
+    interventionName: {
+      type: String,
+      required: true,
+    },
+    interventionDescription: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
