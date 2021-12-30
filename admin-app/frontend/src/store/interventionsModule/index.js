27855e37577732f715dd0c49d3840271c18b329f
Ahmet Polat <polat2000@gmail.com>
2021-12-30 16:31:52 +0100

    init intervention docs

--- admin-app/frontend/src/store/interventionsModule/index.js
+++ admin-app/frontend/src/store/interventionsModule/index.js
@@ -10,6 +10,7 @@ const interventionsModule = {
         description:
           "A kitchen is a room or part of a room used for cooking and food preparation in a dwelling or in a commercial establishment.",
         tags: ["7bbe9c5f-1784-41c5-8795-ae449ec95498"],
+        docs: ["eff8b64c-22e9-4b34-948e-465a3f5b9a4a"],
       },
       {
         interventionId: "59fe15e7-59ad-46bf-a196-cbab81885d5b",
@@ -17,6 +18,7 @@ const interventionsModule = {
         description:
           "A toilet is a piece of sanitary hardware that collects human urine and feces, and sometimes toilet paper, usually for disposal.",
         tags: ["7bbe9c5f-1784-41c5-8795-ae449ec95498"],
+        docs: ["eff8b64c-22e9-4b34-948e-465a3f5b9a4a"],
       },
       {
         interventionId: "c220fb83-a0e4-4463-a28a-f21b260e609a",
@@ -24,6 +26,7 @@ const interventionsModule = {
         description:
           "A plantation is an agricultural estate, generally centered on a plantation house, meant for farming that specializes in cash crops, usually mainly planted with a single crop, with perhaps ancillary areas for vegetables for eating and so on.",
         tags: ["7bbe9c5f-1784-41c5-8795-ae449ec95498"],
+        docs: ["eff8b64c-22e9-4b34-948e-465a3f5b9a4a"],
       },
       {
         interventionId: "5f463cd0-22d3-4321-960d-a4e01efeeccf",
@@ -31,6 +34,7 @@ const interventionsModule = {
         description:
           "Birth control, also known as contraception, anticonception, and fertility control, is a method or device used to prevent pregnancy.",
         tags: ["7bbe9c5f-1784-41c5-8795-ae449ec95498"],
+        docs: ["eff8b64c-22e9-4b34-948e-465a3f5b9a4a"],
       },
       {
         interventionId: "a7dcb0d2-28ca-4679-aa98-f2fc07c66066",
@@ -38,12 +42,19 @@ const interventionsModule = {
         description:
           "Gender equality, also known as sexual equality or equality of the sexes, is the state of equal ease of access to resources and opportunities regardless of gender, including economic participation and decision-making; and the state of valuing different behaviors, aspirations and needs equally, regardless of gender. ",
         tags: ["7bbe9c5f-1784-41c5-8795-ae449ec95498"],
+        docs: [],
       },
     ],
     interventionTags: [
       { tagId: "7bbe9c5f-1784-41c5-8795-ae449ec95498", name: "Tag 1" },
       { tagId: "4b139d0e-1d61-4317-8cc6-1e1364d3b6b9", name: "Tag 2" },
       { tagId: "c1f87fc0-9e7c-400c-aec5-73719a87642a", name: "Tag 3" },
+    ],
+    interventionDocs: [
+      {
+        docId: "eff8b64c-22e9-4b34-948e-465a3f5b9a4a",
+        name: "Some explanatory document",
+      },
     ],
   }),
   getters: {
