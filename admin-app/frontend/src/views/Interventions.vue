078e1a7a9d4a3777ae10f8fd5e64c854c7106cac
Ahmet Polat <a.polat@m3connect.de>
2022-01-05 14:11:34 +0100

    add "add new survey" interaction

--- admin-app/frontend/src/views/Interventions.vue
+++ admin-app/frontend/src/views/Interventions.vue
@@ -4,20 +4,33 @@
     <v-container class="mt-8">
       <v-row class="mr-2 mr-md-0">
         <v-col cols="12" sm="6" md="4" lg="3">
-          <div
-            style="height: 100%"
-            class="pa-2 d-flex justify-center align-center"
-          >
-            <div class="d-flex flex-column align-center">
-              <v-btn
-                fab
-                x-large
-                color="primary lighten-2"
-                @click="clickOnAddNewIntervention"
-              >
-                <v-icon dark> mdi-plus </v-icon>
-              </v-btn>
-              <h2 class="mt-4">New intervention</h2>
+          <div style="height: 100%" class="pa-2">
+            <div
+              style="height: 100%"
+              class="d-flex flex-column justify-space-around"
+            >
+              <div class="d-flex flex-column align-center">
+                <v-btn
+                  fab
+                  x-large
+                  color="primary lighten-2"
+                  @click="clickOnAddNewIntervention"
+                >
+                  <v-icon dark> mdi-wrench </v-icon>
+                </v-btn>
+                <h2 class="mt-2 mb-4">New intervention</h2>
+              </div>
+              <div class="d-flex flex-column align-center">
+                <v-btn
+                  fab
+                  small
+                  color="primary lighten-2"
+                  @click="clickOnAddNewSurvey"
+                >
+                  <v-icon dark> mdi-crosshairs-question </v-icon>
+                </v-btn>
+                <h3 class="mt-2">New Survey</h3>
+              </div>
             </div>
           </div>
         </v-col>
