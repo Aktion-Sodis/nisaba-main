8cbb1fae9b0a0f7df18ba41e9740ea9179ef13ff
Ahmet Polat <a.polat@m3connect.de>
2022-02-08 20:23:26 +0100

    set tab width to 2 in prettier config

--- admin-app/frontend/.prettierrc.js
+++ admin-app/frontend/.prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-    printWidth: 100,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
 };
