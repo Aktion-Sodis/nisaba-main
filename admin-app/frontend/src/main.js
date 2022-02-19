721979ad9a848a591cfb8e404a9a825c5b8670f3
Ahmet Polat <a.polat@m3connect.de>
2022-02-19 21:01:21 +0100

    bind amplify entry to the main Vue instance

--- admin-app/frontend/src/main.js
+++ admin-app/frontend/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import i18n from './i18n';
+
+Amplify.configure(awsconfig);
 
 Vue.config.productionTip = false;
 
