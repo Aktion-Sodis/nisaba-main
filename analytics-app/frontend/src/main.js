72a5ce2ceafe8a93d15f03567acc08b7256cca0b
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-03-04 13:33:23 +0100

    added element-plus design library

--- analytics-app/frontend/src/main.js
+++ analytics-app/frontend/src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(ElementPlus)
 
 app.mount('#app')
