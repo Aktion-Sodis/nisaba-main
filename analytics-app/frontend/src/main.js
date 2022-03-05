9d950b7e12938209c9ceafa732a4a2c06ad129ca
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-03-05 11:40:08 +0100

    added font-awesome

--- analytics-app/frontend/src/main.js
+++ analytics-app/frontend/src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
+
+import '@fortawesome/fontawesome-free/js/all'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
