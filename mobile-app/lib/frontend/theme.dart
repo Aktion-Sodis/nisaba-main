857616d7f9b9e74bd9581e76efaaba1cf99c8483
Arthur Becker <arthur.becker.private@gmail.com>
2022-03-15 13:24:38 +0100

    Added theme colors and default shadow

--- mobile-app/lib/frontend/theme.dart
+++ mobile-app/lib/frontend/theme.dart
@@ -17,3 +17,21 @@ Future<ThemeData> getThemeData() async {
               borderRadius: BorderRadius.all(Radius.circular(8))),
           prefixIconColor: Colors.grey));
 }
+
+class ThemeColors {
+  static const Color black = Colors.black;
+  static const Color yellow = Color.fromARGB(255, 255, 236, 69);
+  static const Color green = Color.fromARGB(255, 75, 185, 126);
+  static const Color darkGrey = Color.fromARGB(255, 72, 70, 73);
+  static const Color red = Color.fromARGB(255, 219, 79, 71);
+  static const Color mobster = Color.fromARGB(
+      255, 120, 117, 121); // actually, light grey with purple tones
+
+}
+
+BoxShadow defaultShadow = BoxShadow(
+  color: Colors.black.withOpacity(0.15),
+  spreadRadius: 1,
+  blurRadius: 2,
+  offset: const Offset(0, 0), // changes position of shadow
+);
