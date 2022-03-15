bc0826ca0fea7eaf0bf767b859da2df73bde3fbd
Arthur Becker <arthur.becker.private@gmail.com>
2022-03-15 13:24:57 +0100

    Custom app bar

---
+++ mobile-app/lib/frontend/components/nisaba_app_bar.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:mobile_app/frontend/dependentsizes.dart';
+import 'package:mobile_app/frontend/theme.dart';
+
+class NisabaAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const NisabaAppBar({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: defaultPadding(context), vertical: 20),
+        child: Row(
+          children: [
+            _backButton(context),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 27),
+            )
+          ],
+        ));
+  }
+
+  @override
+  Size get preferredSize => const Size(double.infinity, 80);
+
+  Widget _backButton(BuildContext context) {
+    if (Navigator.canPop(context)) {
+      return Container(
+        margin: EdgeInsets.only(right: defaultPadding(context)),
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          boxShadow: [defaultShadow],
+        ),
+        child: Material(
+          color: ThemeColors.yellow,
+          shape: const CircleBorder(),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(30),
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              MdiIcons.arrowLeft,
+              size: 30,
+            ),
+          ),
+        ),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+}
