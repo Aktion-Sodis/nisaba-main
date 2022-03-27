3f9cfc0f18588a7035caf49e871ee38d8f5a5c8e
Ahmet Polat <polat2000@gmail.com>
2022-03-27 14:05:29 +0200

    integrate the new 18n calculator into "Intervention"

--- admin-app/frontend/src/components/interventions/Intervention.vue
+++ admin-app/frontend/src/components/interventions/Intervention.vue
@@ -2,14 +2,14 @@
   <v-card style="height: 100%" class="pa-2" outlined tile @click="clickHandler">
     <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"> </v-img>
     <v-card-title>
-      {{ localizedName }}
+      {{ calculateLocalizedString({ languageTexts: interventionName.languageTexts }) }}
       <v-spacer></v-spacer>
       <v-chip v-for="tagId in interventionTagIds" :key="tagId" class="ml-2"
         >{{ tagById({ tagId: tagId }).name }}
       </v-chip>
     </v-card-title>
     <v-card-subtitle class="mt-0">
-      {{ localizedDescription }}
+      {{ calculateLocalizedString({ languageTexts: interventionDescription.languageTexts }) }}
     </v-card-subtitle>
   </v-card>
 </template>
@@ -46,21 +46,8 @@ export default {
   computed: {
     ...mapGetters({
       tagById: 'INTERVENTION_Data/tagById',
+      calculateLocalizedString: 'calculateLocalizedString',
     }),
-    localizedName() {
-      return (
-        this.interventionName.languageTexts[
-          this.interventionName.languageKeys.findIndex((name) => name === this.$i18n.locale)
-        ] ?? ''
-      );
-    },
-    localizedDescription() {
-      return (
-        this.interventionDescription.languageTexts[
-          this.interventionDescription.languageKeys.findIndex((name) => name === this.$i18n.locale)
-        ] ?? ''
-      );
-    },
   },
   methods: {
     clickHandler() {
