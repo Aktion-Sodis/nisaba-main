1354cc059a99f8c7948d5dc54932ae63c99c8565
Ahmet Polat <polat2000@gmail.com>
2022-04-03 18:03:38 +0200

    eslint allow console logs

--- admin-app/frontend/.eslintrc.js
+++ admin-app/frontend/.eslintrc.js
@@ -8,12 +8,13 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': ['error', { 'allow': ['warn', 'error'] }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'max-classes-per-file': 'off',
     'max-len': 'off',
     'import/prefer-default-export': 0,
     'no-underscore-dangle': 'off',
     'no-param-reassign': 'off',
+    'no-console': 'off',
   },
 };
