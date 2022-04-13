bd799cd881fce56e702e71441796aa19b64e93c6
Ahmet Polat <polat2000@gmail.com>
2022-04-13 18:07:11 +0200

    add esc support to DataModal.vue

--- admin-app/frontend/src/components/dataModal/DataModal.vue
+++ admin-app/frontend/src/components/dataModal/DataModal.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-dialog v-model="isDisplayed" max-width="1200px" persistent @keydown.esc="escHandler">
+  <v-dialog
+    v-model="isDisplayed"
+    max-width="1200px"
+    :persistent="!isRead"
+    @keydown.esc="escHandler"
+  >
     <v-card class="px-0 pt-0 px-md-4 pt-md-4">
       <InterventionModalRead v-if="isRead && isIntervention" />
       <InterventionModalForm v-else-if="!isRead && isIntervention" />
@@ -17,7 +22,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapActions, mapGetters } from 'vuex';
 import { dataTypesDict, modalModesDict } from '../../lib/constants';
 
 import InterventionModalForm from './intervention/InterventionModalForm.vue';
@@ -26,6 +31,9 @@ import InterventionModalRead from './intervention/InterventionModalRead.vue';
 import EntityModalForm from './entity/EntityModalForm.vue';
 import EntityModalRead from './entity/EntityModalRead.vue';
 
+import LevelModalForm from './level/LevelModalForm.vue';
+import LevelModalRead from './level/LevelModalRead.vue';
+
 export default {
   name: 'DataModal',
   components: {
@@ -33,6 +41,8 @@ export default {
     InterventionModalRead,
     EntityModalForm,
     EntityModalRead,
+    LevelModalForm,
+    LevelModalRead,
   },
   computed: {
     ...mapGetters({
@@ -55,6 +65,18 @@ export default {
     },
     isSurvey() {
       return this.dataType === dataTypesDict.survey;
+    },
+  },
+  methods: {
+    ...mapActions({
+      abortCreateData: 'dataModal/abortCreateData',
+      abortEditData: 'dataModal/abortEditData',
+      abortReadData: 'dataModal/abortReadData',
+    }),
+    escHandler() {
+      if (this.mode === modalModesDict.edit) this.abortEditData();
+      else if (this.isRead) this.abortCreateData();
+      else this.abortReadData();
     },
   },
 };
