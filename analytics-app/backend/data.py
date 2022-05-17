b425622aeecc92ed23fc7f1a01921bfe95bf1829
benjaminsimeon <benjamin.hiss@rwth-aachen.de>
2022-05-17 17:46:52 -0400

    added humission data

--- analytics-app/backend/data.py
+++ analytics-app/backend/data.py
@@ -1,5 +1,7 @@
 import json
+import pandas as pd
 
+FILE = "data/humission/AppliedIntervention/ddkdud35fe52vlz7pfjhvk5ccq.json"
 
 def import_data(FILE):
     f = open(FILE)
@@ -7,3 +9,7 @@ def import_data(FILE):
     f.close()
 
     return data
+
+data = import_data(FILE)
+
+print(data)
