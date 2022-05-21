42d5a4a7afc593b689d38f24fdd59768d86606ce
benjaminsimeon <benjamin.hiss@rwth-aachen.de>
2022-05-20 22:14:17 -0400

    finished language selector

--- analytics-app/frontend/src/components/languageSelector/LanguageSelector.vue
+++ analytics-app/frontend/src/components/languageSelector/LanguageSelector.vue
@@ -2,7 +2,7 @@
   <div class="language-selector-wrapper" @click="toggleLangguageSelector">
     <div class="language-dropbtn">
       <i class="language-icon fa-solid fa-globe"></i>
-      <div class="language-wrapper">{{ $i18n.locale }}</div>
+      <div class="language-wrapper">{{ $t("languages." + $i18n.locale) }}</div>
       <i class="language-icon fa-solid fa-caret-down"></i>
     </div>
     <div class="language-dropdown-content" v-show="!langdropdownCollapsed">
@@ -13,7 +13,12 @@
         :value="locale"
         @click="changeLang(locale)"
       >
-        {{ locale }}
+        <i class="language-icon fa-solid fa-globe"></i>
+        <div class="language-wrapper">{{ $t("languages." + locale) }}</div>
+        <i
+          class="language-icon hidden fa-solid fa-caret-down"
+          style="'display':hidden "
+        ></i>
       </div>
     </div>
   </div>
@@ -36,7 +41,7 @@ export default {
     },
     changeLang(locale) {
       this.$i18n.locale = locale;
-      console.log("test");
+      console.log(this.$i18n.availableLocales);
       return this.$i18n.locale;
     },
   },
@@ -54,7 +59,7 @@ export default {
 }
 
 .language-dropbtn {
-  height: 36px;
+  height: 30px;
   width: 140px;
   display: flex;
   justify-content: space-between;
@@ -69,33 +74,41 @@ export default {
   cursor: pointer;
   text-align: start;
 
-  border: solid 1px black;
   border-radius: 5px;
+  border: 1px solid black;
   background-color: white;
   height: 36px;
 }
 .language-wrapper {
   color: black;
+  width: 70px;
 }
+.language-content {
+  height: 30px;
+  width: 140px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  text-decoration: none;
+  padding: 0 10px;
+  box-sizing: border-box;
+
+  cursor: pointer;
+  text-align: start;
+
+  border-radius: 5px;
+  background-color: white;
+}
+
 .language-dropdown-content {
   background-color: white;
   border: 1px solid black;
   border-radius: 5px;
   max-height: 150px;
-  overflow: scroll;
-}
-.language-content {
+  width: 140px;
+  overflow-x: hidden;
+  overflow-y: scroll;
   box-sizing: border-box;
-  height: 30px;
-  display: flex;
-  align-items: center;
-
-  padding: 0 0 0 30px;
-
-  cursor: pointer;
-  text-align: start;
-
-  border-bottom: 1px solid lightgrey;
 }
 
 .language-content:hover {
@@ -104,5 +117,8 @@ export default {
 
 .language-icon {
   margin: 0 1px;
+}
+.hidden {
+  visibility: hidden;
 }
 </style>
