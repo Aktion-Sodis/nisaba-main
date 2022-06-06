f6c1fa1ef0f7cd4312a6a47c158d7d4eb5e8e411
Arthur Becker <arthur.becker.private@gmail.com>
2022-06-06 02:31:13 +0200

    Improve LevelRepository for new structure of Organization View BloC

--- mobile-app/lib/backend/repositories/LevelRepository.dart
+++ mobile-app/lib/backend/repositories/LevelRepository.dart
@@ -5,6 +5,33 @@ import 'package:mobile_app/backend/storage/image_synch.dart';
 import 'package:mobile_app/models/ModelProvider.dart' as amp;
 
 class LevelRepository {
+  static Future<List<Level>> getAllLevels() async {
+    List<amp.Level> levels = await Amplify.DataStore.query(
+      amp.Level.classType,
+    );
+
+    List<amp.Level> populated =
+        await Future.wait(levels.map((e) => _populate(e)).toList());
+    List<Level> toOrder =
+        populated.map((e) => Level.fromAmplifyModel(e)).toList();
+
+    // Sort in the ascending order
+    List<Level> result = [];
+    int rootIndex =
+        toOrder.indexWhere((element) => element.parentLevelID == null);
+    result.add(toOrder[rootIndex]);
+    toOrder.removeAt(rootIndex);
+
+    while (toOrder.isNotEmpty) {
+      int childIndex = toOrder
+          .indexWhere((element) => element.parentLevelID == result.last.id);
+      result.add(toOrder[childIndex]);
+      toOrder.removeAt(childIndex);
+    }
+
+    return Future.value(result);
+  }
+
   static Future<amp.Level> getAmpLevelByID(String levelID) async {
     List<amp.Level> levels = await Amplify.DataStore.query(amp.Level.classType,
         where: amp.Level.ID.eq(levelID));
