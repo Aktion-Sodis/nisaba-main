92c25d7295ca7bf785cac67b365d1aca93acfcd0
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-09-19 16:55:53 +0200

    added text component

---
+++ analytics-app/frontend/src/components/data/TextComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-wrapper">
+    <div v-for="answer in question.answers" :key="answer">
+      {{ answer }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextComponent",
+  props: ["question"],
+};
+</script>
+
+<style></style>
