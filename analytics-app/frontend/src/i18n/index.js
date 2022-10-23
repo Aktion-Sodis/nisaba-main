ba16a68311ed8cb7aa8f48380c70da5df08429bf
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-10-23 13:17:49 +0200

    .

--- analytics-app/frontend/src/i18n/index.js
+++ analytics-app/frontend/src/i18n/index.js
@@ -1,19 +1,17 @@
 import { createI18n } from "vue-i18n";
 
-import en from './en-US.json';
-import de from './de-DE.json';
-import es from './es-BO.json';
+import messages from '@intlify/unplugin-vue-i18n/messages'
+
+// import en from './en-US.json';
+// import de from './de-DE.json';
+// import es from './es-BO.json';
 
 const i18n = createI18n({
   legacy: false,
-  locale: localStorage.getItem('lang') || 'en',
-  fallbackLocale: 'en',
+  locale: localStorage.getItem('lang') || 'en-US',
+  fallbackLocale: 'en-US',
   globalInjection: true,
-  messages: {
-    en,
-    de,
-    es
-  }
+  messages
 });
 
 export default i18n;
