547180e94d3a3dbc92be0c0e92f6106f8bf9f5fa
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-10-24 22:47:06 +0200

    fixed message import

--- analytics-app/frontend/src/i18n/index.js
+++ analytics-app/frontend/src/i18n/index.js
@@ -1,14 +1,17 @@
 import { createI18n } from "vue-i18n";
 
-import messages from '@intlify/unplugin-vue-i18n/messages'
-
+import en from './en-US.json'
+import de from './de-DE.json'
 
 const i18n = createI18n({
   legacy: false,
-  locale: localStorage.getItem('lang') || import.meta.env.VITE_APP_I18N_LOCALE || 'en-US',
-  fallbackLocale: import.meta.env.VITE_APP_I18N_FALLBACK_LOCALE || 'en-US',
+  locale: localStorage.getItem('lang') || import.meta.env.VITE_I18N_LOCALE || 'en-US',
+  fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || 'en-US',
   globalInjection: true,
-  messages
+  messages: {
+    'en-US': en,
+    'de-DE': de
+  }
 });
 
 export default i18n;
