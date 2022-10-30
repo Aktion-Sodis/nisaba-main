3b10118ac8a59dee9e8b7add6428ccc38177411f
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2022-10-30 10:42:33 +0100

    resolved dependency issues

--- analytics-app/frontend/vite.config.js
+++ analytics-app/frontend/vite.config.js
@@ -5,9 +5,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'url'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,10 +15,6 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()],
-    }),
-    VueI18nPlugin({
-      // locale messages resource pre-compile option
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/**'),
     }),
   ],
   resolve: {
