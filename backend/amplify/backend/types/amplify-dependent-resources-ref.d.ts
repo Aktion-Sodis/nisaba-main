7541b6279f22b0f01de6c464c8df63692dcbe90f
Arthur Becker <arthur.becker@rwth-aachen.de>
2022-12-14 13:05:10 +0100

    Add amplify auth

--- backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ backend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,41 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "userPoolGroups": {
+            "superadminGroupRole": "string",
+            "adminGroupRole": "string",
+            "mobileGroupRole": "string",
+            "analyticsGroupRole": "string"
+        },
+        "authnisaba": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "authnisabaPreTokenGeneration": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "AdminQueries8dc38185": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "AdminQueries": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
