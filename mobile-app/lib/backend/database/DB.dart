7e28e5b67e8db5f84e54b3b83aebf96c438cd407
Arthur Becker <arthur.becker@rwth-aachen.de>
2023-02-26 17:45:43 +0100

    Adjust generic class

--- mobile-app/lib/backend/database/DB.dart
+++ mobile-app/lib/backend/database/DB.dart
@@ -1,14 +1,14 @@
 import 'DBObject.dart';
 import 'Query.dart';
 
-abstract class DB<G extends DBObject> {
-  Future<void> create(G object);
+abstract class DB {
+  Future<void> create<G extends DBObject>(G object);
 
-  Future<void> update(G object);
+  Future<void> update<G extends DBObject>(G object);
 
-  Future<void> delete(G object);
+  Future<void> delete<G extends DBObject>(G object);
 
-  Future<List<G>> get(Query query);
+  Future<List<G>> get<G extends DBObject>(Query query);
 
-  Future<G> getById(String id);
+  Future<G?> getById<G extends DBObject>(String id);
 }
