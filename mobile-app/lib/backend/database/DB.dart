940c9727f30be0adbc93a408ff1a89039ade0a55
Arthur Becker <arthur.becker@rwth-aachen.de>
2023-03-03 12:44:32 +0100

    wrong notation

--- mobile-app/lib/backend/database/DB.dart
+++ mobile-app/lib/backend/database/DB.dart
@@ -18,7 +18,6 @@ abstract class DB<R extends DBModelRegistration> {
   Future<G?> getById<G extends DBObject>(Type modelType, String id);
 
   // TODO: test this for LocalDB, SyncedDB and RemoteDB
-  @override
   Future<void> clear() async {
     List<Type> types = getRegisteredModelTypes();
     for (Type type in types) {
