9380b31c1981d090c008d6680ecf79693b96fa25
Benjamin Hiss <benjamin.hiss@rwth-aachen.de>
2023-07-01 09:23:45 +0200

    added axios

---
+++ analytics-app/frontend/src/axios/backend-api.js
@@ -0,0 +1,21 @@
+import { Auth } from 'aws-amplify';
+import axios from "axios";
+
+export const getRequest = async (path, payload = {}) => {
+  payload["endpoint"] = import.meta.env.VITE_APP_APPSYNC_ENDPOINT;
+  const url = import.meta.env.VITE_APP_BACKEND_URL + path + "?" + new URLSearchParams(payload);
+
+  const token = (await Auth.currentSession()).idToken.jwtToken;
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  };
+  try {
+    const response = await axios.get(url, config);
+    return response.data.res;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
